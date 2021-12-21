c066fccb49596f3ed6e2cc32accc26e23b7b60da
Stephen Reid <stephen@stephenreid.net>
2021-12-21 17:30:56 +0000

    .

--- models/event_facilitation.rb
+++ models/event_facilitation.rb
@@ -12,5 +12,11 @@ class EventFacilitation
     }
   end
 
+  after_save :clear_cache
+  after_destroy :clear_cache
+  def clear_cache
+    Fragment.and(key: %r{/events/#{event_id}}).destroy_all
+  end
+
   validates_uniqueness_of :account, scope: :event
 end
