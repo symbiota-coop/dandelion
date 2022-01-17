059650c3f5b677241d0602d83411d7722071c313
Stephen Reid <stephen@stephenreid.net>
2022-01-17 22:50:23 +0000

    .

--- lib/omniauth_form.rb
+++ lib/omniauth_form.rb
@@ -4,6 +4,7 @@ module OmniAuth
 
     def css
       [
+        %(<meta content="width=device-width, initial-scale=1.0, maximum-scale=0.75, user-scalable=no" name="viewport">),
         %(<link rel="stylesheet" href="/infinite_admin/plugins/bootstrap/bootstrap4/css/bootstrap.css">),
         %(<link rel="stylesheet" href="/stylesheets/app.css">),
         %(<link rel="stylesheet" href="/stylesheets/sign_in_with_ethereum.css">),
