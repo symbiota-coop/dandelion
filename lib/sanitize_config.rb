53c9899a9b85858c7e8e9b9e7d42672b355e7c02
Stephen Reid <stephen@stephenreid.net>
2022-01-20 10:54:22 +0000

    .

--- lib/sanitize_config.rb
+++ lib/sanitize_config.rb
@@ -5,7 +5,12 @@ Sanitize::Config::DANDELION = Sanitize::Config.merge(Sanitize::Config::RELAXED,
                                                          :all => ['class'],
                                                          'oembed' => ['url'],
                                                          'figure' => ['style'],
-                                                         'span' => ['style']
+                                                         'h1' => ['style'],
+                                                         'h2' => ['style'],
+                                                         'h3' => ['style'],
+                                                         'h4' => ['style'],
+                                                         'h5' => ['style'],
+                                                         'h6' => ['style']
                                                        }
                                                      ),
                                                      css: {
