f596886b2109c3f58c14900feccc9f5769232f9c
Stephen Reid <stephen@stephenreid.net>
2022-09-15 16:31:03 +0100

    .

--- models/prediction.rb
+++ models/prediction.rb
@@ -49,4 +49,21 @@ class Prediction
       headers: { 'Authorization': "Token #{ENV['REPLICATE_API_KEY']}", 'Content-Type': 'application/json' }
     )
   end
+
+  after_create :send_notification
+  def send_notification
+    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY'], 'api.eu.mailgun.net'
+    batch_message = Mailgun::BatchMessage.new(mg_client, 'notifications.dandelion.earth')
+
+    batch_message.from 'Dandelion <notifications@dandelion.earth>'
+    batch_message.subject "[Imagine] #{account.name}"
+    batch_message.body_text prompt
+
+    Account.and(admin: true).each do |account|
+      batch_message.add_recipient(:to, account.email, { 'firstname' => (account.firstname || 'there'), 'token' => account.sign_in_token, 'id' => account.id.to_s })
+    end
+
+    batch_message.finalize if ENV['MAILGUN_API_KEY']
+  end
+  handle_asynchronously :send_notification
 end
