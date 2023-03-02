bec87cd8558f8c09eff0ae5fded521136a3e4ddb
Stephen Reid <stephen@stephenreid.net>
2023-03-02 10:44:29 +0000

    .

--- models/organisation.rb
+++ models/organisation.rb
@@ -256,7 +256,7 @@ class Organisation
 
   has_many :organisation_contributions, dependent: :destroy
   def contributable_events
-    events.and(:id.in => Order.and(:value.gt => 0, :event_id.in => events.pluck(:id)).pluck(:event_id))
+    events.and(:opt_out_of_contribution.ne => true, :id.in => Order.and(:value.gt => 0, :event_id.in => events.pluck(:id)).pluck(:event_id))
   end
 
   def self.contribution_requested_per_event_gbp
