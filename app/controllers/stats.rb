801c03746ef55c5efebf07506514a0e1da7d6b70
Stephen Reid <stephen@stephenreid.net>
2023-03-03 10:26:23 +0000

    .

--- app/controllers/stats.rb
+++ app/controllers/stats.rb
@@ -3,6 +3,10 @@ Dandelion::App.controller do
     admins_only!
   end
 
+  get '/stats/events' do
+    erb :'stats/events'
+  end
+
   get '/stats/feedback' do
     @event_feedbacks = EventFeedback.order('created_at desc')
     erb :'stats/feedback'
@@ -14,8 +18,6 @@ Dandelion::App.controller do
   end
 
   get '/stats/organisations' do
-    @tickets = Ticket.and(:created_at.gte => 3.months.ago, :price.gt => 0)
-    @organisations = Organisation.and(:id.in => Event.and(:id.in => @tickets.pluck(:event_id)).pluck(:organisation_id))
     erb :'stats/organisations'
   end
 
@@ -25,17 +27,17 @@ Dandelion::App.controller do
   end
 
   get '/stats/comments' do
-    @comments = Comment.and(:body.ne => nil).order('created_at desc').paginate(page: params[:page], per_page: 50)
+    @comments = Comment.and(:body.ne => nil).order('created_at desc').paginate(page: params[:page], per_page: 20)
     erb :'stats/comments'
   end
 
   get '/stats/accounts' do
-    @accounts = Account.public.order('created_at desc').and(ps_account_id: nil).paginate(page: params[:page], per_page: 50)
+    @accounts = Account.public.order('created_at desc').and(ps_account_id: nil).paginate(page: params[:page], per_page: 20)
     erb :'stats/accounts'
   end
 
   get '/stats/messages' do
-    @messages = Message.order('created_at desc').paginate(page: params[:page], per_page: 50)
+    @messages = Message.order('created_at desc').paginate(page: params[:page], per_page: 20)
     erb :'stats/messages'
   end
 end
