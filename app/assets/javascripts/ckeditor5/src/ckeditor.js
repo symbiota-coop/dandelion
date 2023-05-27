4d9ed5054d21747eb485bb5f77ee886d1fa451f9
Stephen Reid <stephen@stephenreid.net>
2023-05-27 11:39:32 +0100

    .

--- app/assets/javascripts/ckeditor5/src/ckeditor.js
+++ app/assets/javascripts/ckeditor5/src/ckeditor.js
@@ -114,7 +114,8 @@ ClassicEditor.defaultConfig = {
 			{ model: 'paragraphAction', view: { name: 'p', classes: 'action' }, title: 'Paragraph (action)', class: 'ck-heading_paragraph_action', converterPriority: 'high' },
 			{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
 			{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' }
+			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' }
 		]
 	},
 	image: {
