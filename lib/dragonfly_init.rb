310cbe0fafe38f761a95e3a27e81e7e399d07318
Stephen Reid <stephen@stephenreid.net>
2023-06-06 16:10:07 +0100

    .

--- lib/dragonfly_init.rb
+++ lib/dragonfly_init.rb
@@ -1,10 +1,26 @@
 Dragonfly.app.configure do
   plugin :imagemagick
   url_format '/media/:job/:name'
-  datastore :s3, { bucket_name: ENV['S3_BUCKET_NAME'], access_key_id: ENV['S3_ACCESS_KEY'], secret_access_key: ENV['S3_SECRET'], region: ENV['S3_REGION'], url_scheme: 'https' }
+  datastore :s3, {
+    url_scheme: 'https',
+    url_host: ENV['S3_HOST'],
+    bucket_name: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV['S3_ACCESS_KEY'],
+    secret_access_key: ENV['S3_SECRET'],
+    region: ENV['S3_REGION'],
+    fog_storage_options: {
+      endpoint: ENV['S3_ENDPOINT']
+    }
+  }
+
   secret ENV['DRAGONFLY_SECRET']
 
   define_url do |app, job, _opts|
-    app.datastore.url_for((DragonflyJob.find_by(signature: job.signature) || DragonflyJob.create(uid: job.store, signature: job.signature)).uid)
+    if dj = DragonflyJob.find_by(signature: job.signature)
+      "#{ENV['S3_CDN']}/#{dj.uid}"
+    else
+      dj = DragonflyJob.create(uid: job.store, signature: job.signature)
+      app.datastore.url_for(dj.uid)
+    end
   end
 end
