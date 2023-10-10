41040ae3d54ccb5d8f4514f2ec34300262643e48
Stephen Reid <stephen@stephenreid.net>
2023-10-10 12:08:12 +0200

    .

--- lib/date_formats.rb
+++ lib/date_formats.rb
@@ -2,9 +2,10 @@ require 'active_support/core_ext/integer/inflections'
 
 Time::DATE_FORMATS.merge!(
   default: ->(time) { time.to_fs(:date) + ', ' + time.to_fs(:time) },
-  db_local: ->(time) { time.strftime("%Y-%m-%d %H:%M:%S") },
+  db_local: ->(time) { time.strftime('%Y-%m-%d %H:%M:%S') },
   date: ->(time) { time.to_date.to_s },
   no_year: ->(time) { time.to_date.to_fs(:no_year) + ', ' + time.to_fs(:time) },
+  month_year: ->(time) { time.to_date.to_fs(:month_year) + ', ' + time.to_fs(:time) },
   time: ->(time) { time.strftime("#{(t = time.hour % 12) == 0 ? 12 : t}:%M#{time.strftime('%p').downcase}") },
   no_double_zeros: lambda { |time|
                      time.strftime("#{(t = time.hour % 12) == 0 ? 12 : t}#{time.strftime(':%M') unless time.strftime(':%M') == ':00'}#{time.strftime('%p').downcase}")
@@ -13,8 +14,9 @@ Time::DATE_FORMATS.merge!(
 
 Date::DATE_FORMATS.merge!(
   default: ->(date) { date.strftime("%a #{date.day.ordinalize} %b %Y") },
-  db_local: ->(date) { date.strftime("%Y-%m-%d") },
-  no_year: ->(date) { date.strftime("%a #{date.day.ordinalize} %B") }
+  db_local: ->(date) { date.strftime('%Y-%m-%d') },
+  no_year: ->(date) { date.strftime("%a #{date.day.ordinalize} %B") },
+  month_year: ->(date) { date.strftime('%b %Y') }
 )
 
 module ActiveSupport
