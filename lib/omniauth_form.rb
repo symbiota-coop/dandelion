ec3e9e691e7d42be990345fdf528bc23bdfe17c7
Stephen Reid <stephen@stephenreid.net>
2023-11-05 16:52:17 +0000

    .

--- lib/omniauth_form.rb
+++ lib/omniauth_form.rb
@@ -8,7 +8,7 @@ module OmniAuth
         %(<link rel="stylesheet" href="/infinite_admin/plugins/bootstrap/bootstrap4/css/bootstrap.css">),
         %(<link rel="stylesheet" href="/stylesheets/app.css">),
         %(<link rel="stylesheet" href="/stylesheets/sign_in_with_ethereum.css">),
-        %(<script src="/infinite_admin/plugins/jquery/jquery-3.2.1.min.js"></script>),
+        %(<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>),
         %(<script src="/javascripts/sign_in_with_ethereum.js"></script>)
       ].join("\n")
     end
