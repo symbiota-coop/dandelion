0df2b53b66279e335a4187606b305ca04280f92e
Stephen Reid <stephen@stephenreid.net>
2023-11-16 14:37:34 +0000

    .

--- app/controllers/books.rb
+++ app/controllers/books.rb
@@ -6,11 +6,13 @@ Dandelion::App.controller do
 
   get '/books/:slug' do
     @book = Book.find_by(slug: params[:slug])
+    @title = "#{@book.title} by #{@book.book_author.name}"
     erb :'books/book'
   end
 
   get '/books/:slug/:number' do
     @book = Book.find_by(slug: params[:slug])
+    @title = "#{@book.title} by #{@book.book_author.name}"
     @book_chapter = @book.book_chapters.find_by(number: params[:number])
     erb :'books/book_chapter'
   end
