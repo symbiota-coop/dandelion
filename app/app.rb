395a42e55867fd883618be4be94c764450c5c9b9
Stephen Reid <stephen@stephenreid.net>
2024-01-29 15:15:53 +0000

    .

--- app/app.rb
+++ app/app.rb
@@ -189,36 +189,6 @@ module Dandelion
       erb :'emails/notification', locals: { notification: @notification, circle: @notification.circle }, layout: false
     end
 
-    get '/youtube_thumb/:id' do
-      # load base image
-      begin
-        base_image = MiniMagick::Image.open("https://i.ytimg.com/vi/#{params[:id]}/sddefault.jpg")
-      rescue OpenURI::HTTPError
-        begin
-          base_image = MiniMagick::Image.open("https://i.ytimg.com/vi/#{params[:id]}/hqdefault.jpg")
-        rescue OpenURI::HTTPError
-          halt 404
-        end
-      end
-      base_image = base_image.crop('640x360+0+60')
-
-      # load overlay image
-      overlay_image = MiniMagick::Image.open('app/assets/images/youtube.png')
-
-      # calculate coordinates to place the overlay image in the center
-      x_coordinate = (base_image.width - overlay_image.width) / 2
-      y_coordinate = (base_image.height - overlay_image.height) / 2
-
-      # create a new image which is the result of the composite operation
-      result = base_image.composite(overlay_image) do |c|
-        c.compose 'Over' # OverCompositeOp
-        c.geometry "+#{x_coordinate}+#{y_coordinate}" # place overlay image at calculated coordinates
-      end
-
-      content_type 'image/jpeg'
-      result.to_blob
-    end
-
     post '/upload' do
       sign_in_required!
       upload = current_account.uploads.create(file: params[:upload])
@@ -227,10 +197,6 @@ module Dandelion
 
     get '/donate' do
       erb :donate
-    end
-
-    get '/qr/:id', provides: :png do
-      RQRCode::QRCode.new(params[:id]).as_png(border_modules: 0, module_px_size: 5).to_blob
     end
 
     get '/code' do
---
+++ app/controllers/images.rb
@@ -0,0 +1,35 @@
+Dandelion::App.controller do
+  get '/qr', provides: :png do
+    RQRCode::QRCode.new(params[:url]).as_png(border_modules: 0, size: 500).to_blob
+  end
+
+  get '/youtube_thumb/:id' do
+    # load base image
+    begin
+      base_image = MiniMagick::Image.open("https://i.ytimg.com/vi/#{params[:id]}/sddefault.jpg")
+    rescue OpenURI::HTTPError
+      begin
+        base_image = MiniMagick::Image.open("https://i.ytimg.com/vi/#{params[:id]}/hqdefault.jpg")
+      rescue OpenURI::HTTPError
+        halt 404
+      end
+    end
+    base_image = base_image.crop('640x360+0+60')
+
+    # load overlay image
+    overlay_image = MiniMagick::Image.open('app/assets/images/youtube.png')
+
+    # calculate coordinates to place the overlay image in the center
+    x_coordinate = (base_image.width - overlay_image.width) / 2
+    y_coordinate = (base_image.height - overlay_image.height) / 2
+
+    # create a new image which is the result of the composite operation
+    result = base_image.composite(overlay_image) do |c|
+      c.compose 'Over' # OverCompositeOp
+      c.geometry "+#{x_coordinate}+#{y_coordinate}" # place overlay image at calculated coordinates
+    end
+
+    content_type 'image/jpeg'
+    result.to_blob
+  end
+end
