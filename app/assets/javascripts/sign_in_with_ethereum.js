66cd5b15fd0db34b8b6f5b405b704ad4c26e6bd4
Stephen Reid <stephen@stephenreid.net>
2024-02-22 16:05:22 +0100

    .

--- app/assets/javascripts/sign_in_with_ethereum.js
+++ app/assets/javascripts/sign_in_with_ethereum.js
@@ -2,6 +2,6 @@ $(function () {
   document.title = 'Sign in with Ethereum'
   $('button').click()
   let h1 = 'Sign in with Ethereum'
-  if (typeof window.ethereum == 'undefined') { h1 = 'No wallet found.<br /><a style="color: #5363CA" href="https://metamask.io/">Install Metamask</a>' }
+  if (typeof window.ethereum == 'undefined') { h1 = 'No wallet found.<br /><a style="color: #5363CA" href="https://www.coinbase.com/wallet">Install Coinbase Wallet</a>' }
   $('form').before('<div style="height: 50vh; background-size: cover; background-position: center center; background-image: url(/images/ethereum.webp)"></div><h1 style="text-align: center; display: block !important" class="mt-5">' + h1 + '</h1>')
 })
