d38b21e680c47dc62fbfa258a77b533d72be7b4d
Stephen Reid <stephen@stephenreid.net>
2024-06-04 14:25:30 +0200

    .

--- models/organisation.rb
+++ models/organisation.rb
@@ -324,10 +324,14 @@ class Organisation
     0.8
   end
 
+  def contribution_threshold
+    Money.new((contribution_requested_per_event_gbp || Organisation.contribution_requested_per_event_gbp) * 100, 'GBP')
+  end
+
   def update_paid_up
     update_attribute(:paid_up, nil)
     begin
-      update_attribute(:paid_up, contribution_not_required? || contribution_requested < Money.new((contribution_requested_per_event_gbp || Organisation.contribution_requested_per_event_gbp) * 100, 'GBP') || contributable_events.count == 1 || contribution_paid >= (paid_up_fraction || Organisation.paid_up_fraction) * contribution_requested)
+      update_attribute(:paid_up, contribution_not_required? || contribution_requested < contribution_threshold || contributable_events.count == 1 || contribution_paid >= (paid_up_fraction || Organisation.paid_up_fraction) * contribution_requested)
     rescue Money::Bank::UnknownRate, Money::Currency::UnknownCurrency
       update_attribute(:paid_up, true)
     end
