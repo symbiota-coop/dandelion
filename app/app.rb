05758e07f7dc1569294651cb4b0a1a89e6adf227
Stephen Reid <stephen@stephenreid.net>
2024-07-13 15:03:31 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -78,8 +78,8 @@ module Dandelion
         end
       end
       PageView.create(path: request.path, query_string: request.query_string) if File.extname(request.path).blank? && !request.xhr? && !request.is_crawler?
-      @og_desc = 'Find and host regenerative events and co-created gatherings 🧘🏼‍♀️ 🌱 🕺'
-      @og_image = "#{ENV['BASE_URI']}/images/cover2.jpg"
+      @og_desc = 'regenerative - metamodern - participatory - conscious - transformative - holistic - ethical'
+      @og_image = "#{ENV['BASE_URI']}/images/link.jpg"
       current_account.set(last_active: Time.now) if current_account
     end
 
