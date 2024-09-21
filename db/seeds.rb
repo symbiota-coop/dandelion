4d99528af08b17efaf98a5375e653ef62c7d7fab
Stephen Reid <stephen@stephenreid.net>
2024-09-21 19:13:12 +0200

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 require 'factory_bot'
 
-[Account, Event, Organisation, Gathering, Event].map(&:destroy_all)
 FactoryBot.create_list(:account, 2)
 @account = FactoryBot.create(:account, admin: true, name: ENV['SEED_ACCOUNT_NAME'], email: ENV['SEED_ACCOUNT_EMAIL'], password: ENV['SEED_ACCOUNT_PASSWORD'])
-@gathering = FactoryBot.create(:gathering, account: @account, name: 'Nettlecombe', slug: 'nettlecombe')
-@organisation = FactoryBot.create(:organisation, account: @account, name: 'Autopia', slug: 'autopia', paid_up: true)
+@gathering = FactoryBot.create(:gathering, account: @account, name: 'Test Gathering', slug: 'test-gathering')
+@organisation = FactoryBot.create(:organisation, account: @account, name: 'Test Organisation', slug: 'test-organisation', paid_up: true)
 @event = FactoryBot.create_list(:event, 4, organisation: @organisation, account: @account, last_saved_by: @account, image: File.open(Padrino.root('app/assets/images/test-event.jpg')), featured: true)
