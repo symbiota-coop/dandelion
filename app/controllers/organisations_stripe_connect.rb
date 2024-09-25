adc6328990b38c95d03ef68b8820890274be87ce
Stephen Reid <stephen@stephenreid.net>
2024-09-25 17:21:29 +0200

    .

--- app/controllers/organisations_stripe_connect.rb
+++ app/controllers/organisations_stripe_connect.rb
@@ -52,7 +52,7 @@ Dandelion::App.controller do
     sig_header = request.env['HTTP_STRIPE_SIGNATURE']
     begin
       event = Stripe::Webhook.construct_event(
-        payload, sig_header, ENV['STRIPE_CONNECT_ENDPOINT_SECRET']
+        payload, sig_header, ENV['STRIPE_ENDPOINT_SECRET_CONNECT']
       )
     rescue JSON::ParserError
       halt 400
