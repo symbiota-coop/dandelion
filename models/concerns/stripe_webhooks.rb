be9881ef847e7dc4497edad8affcf39db72bd341
Stephen Reid <stephen@stephenreid.net>
2024-10-07 14:43:14 +0200

    .

--- models/concerns/stripe_webhooks.rb
+++ models/concerns/stripe_webhooks.rb
@@ -32,7 +32,7 @@ module StripeWebhooks
   def delete_stripe_webhook
     Stripe.api_key = stripe_sk
     Stripe.api_version = '2020-08-27'
-    webhook = stripe_webhooks.find { |w| w['url'] == stripe_webhook_url && w['enabled_events'].include?('checkout.session.completed') }
+    webhook = stripe_webhooks.find { |w| w['url'] == stripe_webhook_url }
     Stripe::WebhookEndpoint.delete(webhook.id) if webhook
   end
 
@@ -42,7 +42,7 @@ module StripeWebhooks
     Stripe.api_key = stripe_sk
     Stripe.api_version = '2020-08-27'
 
-    return if stripe_webhooks.find { |w| w['url'] == stripe_webhook_url && w['enabled_events'].include?('checkout.session.completed') }
+    return if stripe_webhooks.find { |w| w['url'] == stripe_webhook_url }
 
     w = Stripe::WebhookEndpoint.create({
                                          url: stripe_webhook_url,
