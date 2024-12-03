149804ba943df283ab331b9bfb294427744ab21b
Stephen Reid <stephen@stephenreid.net>
2024-12-03 09:26:34 +0100

    🚸 Update 'Name' field label to 'Full name' for improved clarity

--- test/accounts_test.rb
+++ test/accounts_test.rb
@@ -6,7 +6,7 @@ class DandelionTest < ActiveSupport::TestCase
   test 'signing up' do
     @account = FactoryBot.build_stubbed(:account)
     visit '/accounts/new'
-    fill_in 'Name', with: @account.name
+    fill_in 'Full name', with: @account.name
     fill_in 'Email', with: @account.email
     fill_in 'Location', with: @account.location
     click_button 'Sign up'
@@ -27,7 +27,7 @@ class DandelionTest < ActiveSupport::TestCase
     login_as(@account)
     click_link @account.name
     click_link 'Edit profile'
-    fill_in 'Name', with: (name = FactoryBot.build_stubbed(:account).name)
+    fill_in 'Full name', with: (name = FactoryBot.build_stubbed(:account).name)
     click_button 'Save profile'
     assert page.has_content? 'Your account was updated successfully'
     assert page.has_content? name
