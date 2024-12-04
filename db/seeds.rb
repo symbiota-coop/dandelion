3477db029b42d203b4d3ad78711faafed6fb77d1
Stephen Reid <stephen@stephenreid.net>
2024-12-04 13:23:59 +0100

    🔥 Remove unused instance variable @gathering
    ♻️ Refactor seed data creation for improved clarity and consistency
    
    🚚 Move gathering creation to the end of the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ require 'factory_bot'
 
 FactoryBot.create_list(:account, 2)
 @account = FactoryBot.create(:account, admin: true, name: ENV['SEED_ACCOUNT_NAME'], email: ENV['SEED_ACCOUNT_EMAIL'], password: ENV['SEED_ACCOUNT_PASSWORD'])
-@gathering = FactoryBot.create(:gathering, account: @account, name: 'Test Gathering', slug: 'test-gathering')
 @organisation = FactoryBot.create(:organisation, account: @account, name: 'Test Organisation', slug: 'test-organisation', paid_up: true)
-@event = FactoryBot.create_list(:event, 4, organisation: @organisation, account: @account, last_saved_by: @account, image: File.open(Padrino.root('app/assets/images/test-event.jpg')), featured: true)
+FactoryBot.create_list(:event, 4, organisation: @organisation, account: @account, last_saved_by: @account, image: File.open(Padrino.root('app/assets/images/test-event.jpg')), featured: true)
+FactoryBot.create(:gathering, account: @account, name: 'Test Gathering', slug: 'test-gathering')
