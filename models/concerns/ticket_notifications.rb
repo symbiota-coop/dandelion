71292b7cab9ae9c8a6560c463b3239b8e032467f
Stephen Reid <stephen@stephenreid.net>
2024-12-10 13:47:27 +0100

    ♻️ Refactor ticket notifications into a separate concern
    This commit refactors the ticket notification logic into a separate
    concern to improve code organization and maintainability. It also adds
    a new feature to send notifications for ticket resales.

---
+++ models/concerns/ticket_notifications.rb
@@ -0,0 +1,100 @@
+module TicketNotifications
+  extend ActiveSupport::Concern
+
+  included do
+    handle_asynchronously :send_ticket
+  end
+
+  def send_ticket
+    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY'], ENV['MAILGUN_REGION']
+    batch_message = Mailgun::BatchMessage.new(mg_client, ENV['MAILGUN_TICKETS_HOST'])
+
+    order = event.orders.new
+    order.account = account
+    account = self.account
+    order.tickets = [self]
+
+    content = ERB.new(File.read(Padrino.root('app/views/emails/tickets.erb'))).result(binding)
+    batch_message.subject(event.ticket_email_title || "Ticket to #{event.name}")
+
+    if event.organisation.send_ticket_emails_from_organisation && event.organisation.reply_to && event.organisation.image
+      header_image_url = event.organisation.image.url
+      batch_message.from event.organisation.reply_to
+      batch_message.reply_to event.email
+    else
+      header_image_url = "#{ENV['BASE_URI']}/images/black-on-transparent-sq.png"
+      batch_message.from ENV['TICKETS_EMAIL_FULL']
+      batch_message.reply_to(event.email || event.organisation.reply_to)
+    end
+
+    batch_message.body_html Premailer.new(ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding), with_html_string: true, adapter: 'nokogiri', input_encoding: 'UTF-8').to_inline_css
+
+    unless event.no_tickets_pdf
+      tickets_pdf_filename = "dandelion-#{event.name.parameterize}-#{order.id}.pdf"
+      tickets_pdf_file = File.new(tickets_pdf_filename, 'w+')
+      tickets_pdf_file.write order.tickets_pdf.render
+      tickets_pdf_file.rewind
+      batch_message.add_attachment tickets_pdf_file, tickets_pdf_filename
+    end
+
+    cal = event.ical
+    ics_filename = "dandelion-#{event.name.parameterize}-#{order.id}.ics"
+    ics_file = File.new(ics_filename, 'w+')
+    ics_file.write cal.to_ical
+    ics_file.rewind
+    batch_message.add_attachment ics_file, ics_filename
+
+    [account].each do |account|
+      batch_message.add_recipient(:to, account.email, { 'firstname' => account.firstname || 'there', 'token' => account.sign_in_token, 'id' => account.id.to_s })
+    end
+
+    batch_message.finalize if ENV['MAILGUN_API_KEY']
+
+    unless event.no_tickets_pdf
+      tickets_pdf_file.close
+      File.delete(tickets_pdf_filename)
+    end
+    ics_file.close
+    File.delete(ics_filename)
+  end
+
+  def notify_of_failed_refund(error)
+    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY'], ENV['MAILGUN_REGION']
+    batch_message = Mailgun::BatchMessage.new(mg_client, ENV['MAILGUN_NOTIFICATIONS_HOST'])
+
+    ticket = self
+    order = ticket.order
+    event = order.event
+    account = order.account
+    content = ERB.new(File.read(Padrino.root('app/views/emails/refund_failed_ticket.erb'))).result(binding)
+    batch_message.from ENV['NOTIFICATIONS_EMAIL_FULL']
+    batch_message.subject "Refund failed: #{account.name} in #{event.name}"
+    batch_message.body_html Premailer.new(ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding), with_html_string: true, adapter: 'nokogiri', input_encoding: 'UTF-8').to_inline_css
+
+    event.event_facilitators.each do |account|
+      batch_message.add_recipient(:to, account.email, { 'firstname' => account.firstname || 'there', 'token' => account.sign_in_token, 'id' => account.id.to_s })
+    end
+
+    batch_message.finalize if ENV['MAILGUN_API_KEY']
+  end
+
+  def send_resale_notification
+    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY'], ENV['MAILGUN_REGION']
+    batch_message = Mailgun::BatchMessage.new(mg_client, ENV['MAILGUN_NOTIFICATIONS_HOST'])
+
+    ticket = self
+    order = ticket.order
+    event = order.event
+    account = order.account
+    content = ERB.new(File.read(Padrino.root('app/views/emails/ticket_resale.erb'))).result(binding)
+    batch_message.from ENV['NOTIFICATIONS_EMAIL_FULL']
+    batch_message.subject "Ticket resale: #{account.name} in #{event.name}"
+    batch_message.body_html Premailer.new(ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding), with_html_string: true, adapter: 'nokogiri', input_encoding: 'UTF-8').to_inline_css
+
+    event.event_facilitators.each do |account|
+      batch_message.add_recipient(:to, account.email, { 'firstname' => account.firstname || 'there', 'token' => account.sign_in_token, 'id' => account.id.to_s })
+    end
+
+    batch_message.finalize if ENV['MAILGUN_API_KEY']
+  end
+end
--- models/ticket.rb
+++ models/ticket.rb
@@ -3,6 +3,8 @@ class Ticket
   include Mongoid::Timestamps
   include Mongoid::Paranoia
 
+  include TicketNotifications
+
   belongs_to :event, index: true
   belongs_to :account, index: true, optional: true
   belongs_to :order, index: true, optional: true
@@ -94,6 +96,7 @@ class Ticket
     if event.enable_resales? && ticket_type.remaining_including_made_available < 0 && (ticket = ticket_type.tickets.and(:made_available_at.ne => nil).order('made_available_at asc').first)
       ticket.refund
       ticket.destroy
+      send_resale_notification
     end
     event.waitships.find_by(account: account).try(:destroy)
     event.gathering.memberships.create(account: account, unsubscribed: true) if event.gathering
@@ -169,60 +172,6 @@ class Ticket
     zoomship.update_attribute(:tickets_count, zoomship.tickets.count)
   end
 
-  def send_ticket
-    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY'], ENV['MAILGUN_REGION']
-    batch_message = Mailgun::BatchMessage.new(mg_client, ENV['MAILGUN_TICKETS_HOST'])
-
-    order = event.orders.new
-    order.account = account
-    account = self.account
-    order.tickets = [self]
-
-    content = ERB.new(File.read(Padrino.root('app/views/emails/tickets.erb'))).result(binding)
-    batch_message.subject(event.ticket_email_title || "Ticket to #{event.name}")
-
-    if event.organisation.send_ticket_emails_from_organisation && event.organisation.reply_to && event.organisation.image
-      header_image_url = event.organisation.image.url
-      batch_message.from event.organisation.reply_to
-      batch_message.reply_to event.email
-    else
-      header_image_url = "#{ENV['BASE_URI']}/images/black-on-transparent-sq.png"
-      batch_message.from ENV['TICKETS_EMAIL_FULL']
-      batch_message.reply_to(event.email || event.organisation.reply_to)
-    end
-
-    batch_message.body_html Premailer.new(ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding), with_html_string: true, adapter: 'nokogiri', input_encoding: 'UTF-8').to_inline_css
-
-    unless event.no_tickets_pdf
-      tickets_pdf_filename = "dandelion-#{event.name.parameterize}-#{order.id}.pdf"
-      tickets_pdf_file = File.new(tickets_pdf_filename, 'w+')
-      tickets_pdf_file.write order.tickets_pdf.render
-      tickets_pdf_file.rewind
-      batch_message.add_attachment tickets_pdf_file, tickets_pdf_filename
-    end
-
-    cal = event.ical
-    ics_filename = "dandelion-#{event.name.parameterize}-#{order.id}.ics"
-    ics_file = File.new(ics_filename, 'w+')
-    ics_file.write cal.to_ical
-    ics_file.rewind
-    batch_message.add_attachment ics_file, ics_filename
-
-    [account].each do |account|
-      batch_message.add_recipient(:to, account.email, { 'firstname' => account.firstname || 'there', 'token' => account.sign_in_token, 'id' => account.id.to_s })
-    end
-
-    batch_message.finalize if ENV['MAILGUN_API_KEY']
-
-    unless event.no_tickets_pdf
-      tickets_pdf_file.close
-      File.delete(tickets_pdf_filename)
-    end
-    ics_file.close
-    File.delete(ics_filename)
-  end
-  handle_asynchronously :send_ticket
-
   def refund
     return unless event.refund_deleted_orders && event.organisation && discounted_price && discounted_price > 0 && payment_completed && payment_intent
 
@@ -246,25 +195,5 @@ class Ticket
   rescue Stripe::InvalidRequestError => e
     notify_of_failed_refund(e)
     true
-  end
-
-  def notify_of_failed_refund(error)
-    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY'], ENV['MAILGUN_REGION']
-    batch_message = Mailgun::BatchMessage.new(mg_client, ENV['MAILGUN_NOTIFICATIONS_HOST'])
-
-    ticket = self
-    order = ticket.order
-    event = order.event
-    account = order.account
-    content = ERB.new(File.read(Padrino.root('app/views/emails/refund_failed_ticket.erb'))).result(binding)
-    batch_message.from ENV['NOTIFICATIONS_EMAIL_FULL']
-    batch_message.subject "Refund failed: #{account.name} in #{event.name}"
-    batch_message.body_html Premailer.new(ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding), with_html_string: true, adapter: 'nokogiri', input_encoding: 'UTF-8').to_inline_css
-
-    event.event_facilitators.each do |account|
-      batch_message.add_recipient(:to, account.email, { 'firstname' => account.firstname || 'there', 'token' => account.sign_in_token, 'id' => account.id.to_s })
-    end
-
-    batch_message.finalize if ENV['MAILGUN_API_KEY']
   end
 end
