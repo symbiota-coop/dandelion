ef9bf2fafd3d55b5bdf42392f86adf3c49a11c51
Stephen Reid <stephen@stephenreid.net>
2025-02-05 15:24:06 +0100

    🌐 Add EventTag data to daily view for AJAX requests
    This commit enhances the '/daily' route to include EventTag data when
    responding to AJAX requests. This change allows for dynamic loading of
    event tags, improving the user experience in the daily view.

--- app/controllers/daily.rb
+++ app/controllers/daily.rb
@@ -2,6 +2,7 @@ Dandelion::App.controller do
   get '/daily' do
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     if request.xhr?
+      @event_tags = EventTag.all
       cp(:daily, key: '/daily', expires: 1.day.from_now)
     else
       erb :daily
