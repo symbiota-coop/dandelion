5c6049ba400abadb86054881b0610b2c61855721
Stephen Reid <stephen@stephenreid.net>
2025-03-14 12:10:25 +0200

    ✨ (app.js): add function to get currency symbol from currency code 🎨 (app/views): use javascript to dynamically update currency symbol The currency symbol was previously hardcoded in the views. This commit adds a javascript function to get the currency symbol from the currency code and uses this function to dynamically update the currency symbol in the views. This allows the currency symbol to be updated when the currency is changed.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,20 @@ function nl2br (str) {
   return str.replace(/(?:\r\n|\r|\n)/g, '<br>')
 }
 
+function getCurrencySymbol (currency) {
+  try {
+    const parts = new Intl.NumberFormat('en', { style: 'currency', currency: currency })
+      .formatToParts();
+    const symbol = parts.find(part => part.type === 'currency');
+    const symbolValue = symbol ? symbol.value : currency;
+
+    // If symbol is compound (>1 char) and ends with $, return original currency code
+    return (symbolValue.length > 1 && symbolValue.endsWith('$')) ? currency : symbolValue;
+  } catch (e) {
+    return currency;
+  }
+}
+
 $.fn.serializeObject = function () {
   const o = {};
   const a = this.serializeArray();
