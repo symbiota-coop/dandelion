dc972b300f7005baead250ffbcd3ba2a7eadcb24
Stephen Reid <stephen@stephenreid.net>
2025-05-01 20:14:12 +0200

    ✨ Notify global admins of critical failures
    Include all accounts with `admin: true` in recipient lists for
    Stripe error and refund failure notifications.
    
    This ensures critical system alerts are always delivered to a core
    group of administrators for better monitoring and response.

--- models/concerns/order_notifications.rb
+++ models/concerns/order_notifications.rb
@@ -103,7 +103,7 @@ module OrderNotifications
     batch_message.subject "Stripe error on #{event.name}"
     batch_message.body_html Premailer.new(ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding), with_html_string: true, adapter: 'nokogiri', input_encoding: 'UTF-8').to_inline_css
 
-    event.organisation.admins_receiving_feedback.each do |account|
+    (event.organisation.admins_receiving_feedback + Account.and(admin: true)).uniq.each do |account|
       batch_message.add_recipient(:to, account.email, { 'firstname' => account.firstname || 'there', 'token' => account.sign_in_token, 'id' => account.id.to_s })
     end
 
@@ -122,7 +122,7 @@ module OrderNotifications
     batch_message.subject "Refund failed: #{account.name} in #{event.name}"
     batch_message.body_html Premailer.new(ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding), with_html_string: true, adapter: 'nokogiri', input_encoding: 'UTF-8').to_inline_css
 
-    event.event_facilitators.each do |account|
+    (event.event_facilitators + Account.and(admin: true)).uniq.each do |account|
       batch_message.add_recipient(:to, account.email, { 'firstname' => account.firstname || 'there', 'token' => account.sign_in_token, 'id' => account.id.to_s })
     end
 
