76e4042f277e1d2cf9e955dae5d0c3db3d06f301
Stephen Reid <stephen@stephenreid.net>
2025-06-01 18:30:48 +0200

    🦺 Re-enable and refine event uniqueness validation Re-enables uniqueness validation for event name, start time, and organization. Adds `unless: :duplicate` to allow intentional event duplication, preventing accidental duplicates while supporting workflow.

--- models/concerns/event_validation.rb
+++ models/concerns/event_validation.rb
@@ -4,8 +4,7 @@ module EventValidation
   included do
     validates_presence_of :name, :start_time, :end_time, :location, :currency
     validates_uniqueness_of :slug, allow_nil: true
-    # needs rethinking as this prevents duplicating events!
-    # validates_uniqueness_of :name, scope: [:start_time, :organisation_id], message: 'is invalid: an event with this title and start time already exists for this organisation'
+    validates_uniqueness_of :name, scope: [:start_time, :organisation_id], message: 'is invalid: an event with this title and start time already exists for this organisation', unless: :duplicate
     validates_format_of :slug, with: /\A[a-z0-9-]+\z/, if: :slug
 
     before_validation do
