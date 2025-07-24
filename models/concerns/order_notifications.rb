ff089cf3270b660c1909c2363d1c0fc4719d5df4
Stephen Reid <stephen@stephenreid.net>
2025-07-24 23:09:57 +0200

    ✨ Implement ticket resend functionality
    Adds a new `send_tickets_again` method to the `OrderNotifications` concern.
    This method allows for re-sending ticket emails to customers, which is
    useful for cases where the initial email might have been missed or
    a customer requests a resend.
    
    The new method is handled asynchronously to prevent blocking the main
    application thread during email delivery. It also includes a distinct
    subject line to clearly indicate that the email is a resend.

--- models/concerns/order_notifications.rb
+++ models/concerns/order_notifications.rb
@@ -4,6 +4,7 @@ module OrderNotifications
   included do
     handle_asynchronously :send_notification
     handle_asynchronously :send_tickets
+    handle_asynchronously :send_tickets_again
   end
 
   def send_notification
@@ -36,6 +37,75 @@ module OrderNotifications
     content = ERB.new(File.read(Padrino.root('app/views/emails/tickets.erb'))).result(binding)
     batch_message.subject(
       ((event.recording? ? event.recording_email_title : event.ticket_email_title) || (event.recording? ? event.organisation.recording_email_title : event.organisation.ticket_email_title))
+      .gsub('[ticket_or_tickets]', tickets.count == 1 ? 'Ticket' : 'Tickets')
+      .gsub('[event_name]', event.name)
+    )
+
+    if event.organisation.send_ticket_emails_from_organisation && event.organisation.image
+      header_image_url = event.organisation.image.thumb('1920x1920').url
+      batch_message.from "#{event.organisation.name} <#{ENV['TICKETS_EMAIL']}>"
+    else
+      header_image_url = "#{ENV['BASE_URI']}/images/black-on-transparent-sq.png"
+      batch_message.from ENV['TICKETS_EMAIL_FULL']
+    end
+    batch_message.reply_to(event.email || event.organisation.reply_to)
+
+    batch_message.body_html Premailer.new(ERB.new(File.read(Padrino.root('app/views/layouts/email.erb'))).result(binding), with_html_string: true, adapter: 'nokogiri', input_encoding: 'UTF-8').to_inline_css
+
+    unless event.no_tickets_pdf
+      tickets_pdf_filename = "dandelion-#{event.name.parameterize}-#{order.id}.pdf"
+      tickets_pdf_file = File.new(tickets_pdf_filename, 'w+')
+      tickets_pdf_file.write order.tickets_pdf.render
+      tickets_pdf_file.rewind
+      batch_message.add_attachment tickets_pdf_file, tickets_pdf_filename
+    end
+
+    if event.event_sessions.empty?
+      cal = event.ical(order: order)
+      ics_filename = "dandelion-#{event.name.parameterize}-#{order.id}.ics"
+      ics_file = File.new(ics_filename, 'w+')
+      ics_file.write cal.to_ical
+      ics_file.rewind
+      batch_message.add_attachment ics_file, ics_filename
+    else
+      event.event_sessions.each do |event_session|
+        cal = event_session.ical(order: order)
+        ics_filename = "dandelion-#{event_session.name.parameterize}-#{order.id}.ics"
+        ics_file = File.new(ics_filename, 'w+')
+        ics_file.write cal.to_ical
+        ics_file.rewind
+        batch_message.add_attachment ics_file, ics_filename
+      end
+    end
+
+    [account].each do |account|
+      batch_message.add_recipient(:to, account.email, { 'firstname' => account.firstname || 'there', 'token' => account.sign_in_token, 'id' => account.id.to_s })
+    end
+
+    if ENV['MAILGUN_API_KEY']
+      message_ids = batch_message.finalize
+      update_attribute(:message_ids, message_ids)
+    end
+
+    unless event.no_tickets_pdf
+      tickets_pdf_file.close
+      File.delete(tickets_pdf_filename)
+    end
+    ics_file.close
+    File.delete(ics_filename)
+  end
+
+  def send_tickets_again
+    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY'], ENV['MAILGUN_REGION']
+    batch_message = Mailgun::BatchMessage.new(mg_client, ENV['MAILGUN_TICKETS_HOST'])
+
+    order = self
+    event = order.event
+
+    account = order.account
+    content = ERB.new(File.read(Padrino.root('app/views/emails/tickets.erb'))).result(binding)
+    batch_message.subject(
+      ('Resending with ticket PDF: ' + ((event.recording? ? event.recording_email_title : event.ticket_email_title) || (event.recording? ? event.organisation.recording_email_title : event.organisation.ticket_email_title)))
       .gsub('[ticket_or_tickets]', tickets.count == 1 ? 'Ticket' : 'Tickets')
       .gsub('[event_name]', event.name)
     )
