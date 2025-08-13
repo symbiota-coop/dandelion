216f38dc7d30f03d277bc98a37f6355748ab55d4
Stephen Reid <stephen@stephenreid.net>
2025-08-13 12:18:30 +0200

    💄 Add GoCardless payment method and update landing page copy 🍱 Add GoCardless logo asset 💬 Update OG description to include "and host" 🎨 Improve layout and styling for payment methods section

--- app/app.rb
+++ app/app.rb
@@ -50,7 +50,7 @@ module Dandelion
         sign_in_via_api_key
       end
       PageView.create(path: request.path, query_string: request.query_string) if File.extname(request.path).blank? && !request.xhr? && !request.is_crawler? && !request.path.start_with?('/z/')
-      @og_desc = "Find #{%w[soulful regenerative metamodern participatory conscious transformative holistic ethical].join(' · ')} events and co-created gatherings"
+      @og_desc = "Find and host #{%w[soulful regenerative metamodern participatory conscious transformative holistic ethical].join(' · ')} events and co-created gatherings"
       @og_image = "#{ENV['BASE_URI']}/images/link.jpg"
       if current_account
         current_account.set(last_active: Time.now)
