0d8157246ff2968188b9439b90a1241ace908c6b
Stephen Reid <stephen@stephenreid.net>
2025-09-06 08:38:35 +0200

    ✨ Add dedicated JS file for event-build page to improve caching

---
+++ app/assets/javascripts/events_build.js
@@ -0,0 +1,307 @@
+/* global buildShowIntro, buildDraft, buildNewRecord, buildTimeZoneSuffix, buildImageRequiredWidth, buildImageRequiredHeight, buildIsOrgAdmin, buildRevenueSharingEnabled, eventId */
+
+$(function () {
+  // Intro tour on new org creation
+  if (buildShowIntro && $(window).width() > 992) {
+    if (typeof introJs !== 'undefined') {
+      introJs().setOptions({
+        steps: [{
+          title: 'Great job!',
+          intro: "Now let's create an event under your new organisation."
+        }]
+      }).start()
+    }
+  }
+
+  // Load draft into form fields (including datetime and wysiwyg)
+  if (buildDraft) {
+    const draft = buildDraft
+    const form = $('#build-event')[0]
+
+    // Set regular form values
+    $.each(draft, function (key, value) {
+      if (value) {
+        $(form).find('[name="event[' + key + ']"]').val(value)
+      }
+    })
+
+    // Set flatpickrs for datetime fields
+    $(form).find('input.datetimepicker').each(function () {
+      const field = $(this)
+      const fieldName = field.attr('name')
+      if (fieldName) {
+        const match = fieldName.match(/event\[(.+)\]/)
+        if (match && draft[match[1]]) {
+          field.flatpickr({
+            altInput: true,
+            altFormat: 'J F Y, H:i',
+            enableTime: true,
+            time_24hr: true
+          })
+        }
+      }
+    })
+
+    // Set CKEditor content
+    $(form).find('textarea.wysiwyg').each(function () {
+      const field = $(this)
+      const fieldName = field.attr('name')
+      if (fieldName) {
+        const match = fieldName.match(/event\[(.+)\]/)
+        if (match && draft[match[1]]) {
+          const editorInstance = field.next().find('[contenteditable]')[0].ckeditorInstance
+          editorInstance.setData(draft[match[1]])
+        }
+      }
+    })
+  }
+
+  // Autosave draft + Next buttons on new record
+  if (buildNewRecord) {
+    const draftInterval = setInterval(function () {
+      if ($('#event_name').val().length > 0) {
+        $.post('/events/draft', $('#build-event').serializeObject())
+      }
+    }, 10 * 1000)
+    $('#build-event').submit(function () {
+      clearInterval(draftInterval)
+    })
+
+    $('.tab-pane').each(function () {
+      const tabPane = this
+      $('<a href="javascript:;" class="next btn btn-primary">Next</a>').appendTo(tabPane)
+      $(this).find('a.next').click(function () {
+        const nextTab = $(tabPane).next().attr('id')
+        $('#event-build-nav a[href="#' + nextTab + '"]').tab('show')
+
+        // Scroll the tab into view
+        const navWrapper = $('.nav-wrapper')[0]
+        const activeTab = $('#event-build-nav a[href="#' + nextTab + '"]')[0]
+        if (navWrapper && activeTab) {
+          navWrapper.scrollLeft = activeTab.offsetLeft - (navWrapper.clientWidth / 2) + (activeTab.clientWidth / 2)
+        }
+      })
+    })
+    $('.tab-pane:last').find('a.next').remove()
+  }
+
+  // Email labels tweaks
+  $('label[for$="_email_greeting"], label[for$="_email_body"]').hide()
+  $('label[for$="_email_title"]').each(function () {
+    $(this).text($(this).text().replace('email subject', 'email'))
+  })
+
+  // Questions textareas and autosize
+  $('#event_questions').attr('rows', '8')
+  $('#event_feedback_questions').attr('rows', '8')
+  if (typeof autosize !== 'undefined') {
+    if ($('#event_questions')[0]) autosize($('#event_questions')[0])
+    if ($('#event_feedback_questions')[0]) autosize($('#event_feedback_questions')[0])
+  }
+
+  // Validate on tab change and keep textareas sized
+  $('#event-build-nav a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+    setTimeout(function () {
+      if (typeof autosize !== 'undefined') {
+        if ($('#event_questions')[0]) autosize.update($('#event_questions')[0])
+        if ($('#event_feedback_questions')[0]) autosize.update($('#event_feedback_questions')[0])
+      }
+    }, 0)
+
+    const form = $('#event-build-nav').closest('form')[0]
+    if (form.reportValidity()) {
+      // continue
+    } else {
+      e.preventDefault()
+      $(window).scrollTop($(form).find(':invalid').first().offset().top - $('#header').height() - 36)
+      $(form).find(':invalid').first().focus()
+    }
+  })
+
+  // Timezone hint
+  if (buildTimeZoneSuffix) {
+    $('#event_start_time').siblings('small').text(buildTimeZoneSuffix)
+    $('#event_end_time').siblings('small').text(buildTimeZoneSuffix)
+  }
+
+  // Google Places Autocomplete for location
+  if (typeof google !== 'undefined') {
+    const autocomplete = new google.maps.places.Autocomplete($('#event_location')[0])
+    $('#event_location').keydown(function (e) {
+      if (e.which === 13 && $('.pac-container:visible').length) return false
+    })
+  }
+
+  // Ensure end_time can't be before start_time
+  $('#event_start_time').change(function () {
+    const startPicker = $('#event_start_time')[0]._flatpickr
+    const endPicker = $('#event_end_time')[0]._flatpickr
+    if (startPicker && endPicker) {
+      endPicker.set('minDate', startPicker.selectedDates[0])
+    }
+  })
+
+  // Image validation and hints
+  $('#event_image').change(function () {
+    const fileUpload = this
+    const reader = new FileReader()
+
+    reader.readAsDataURL(fileUpload.files[0])
+    reader.onload = function (e) {
+      const image = new Image()
+      image.src = e.target.result
+      image.onload = function () {
+        const height = this.height
+        const width = this.width
+
+        if (width < 992) {
+          alert('Please use an image that is at least 992px wide')
+          $(fileUpload).val('')
+        }
+        if (width > 7680) {
+          alert('Please use an image that is less than 7680px wide')
+          $(fileUpload).val('')
+        }
+        if (height > width) {
+          alert('Please use an image that is more wide than high')
+          $(fileUpload).val('')
+        }
+
+        if (buildImageRequiredWidth) {
+          if (width !== buildImageRequiredWidth) {
+            alert('Please use an image that is ' + buildImageRequiredWidth + 'px wide')
+            $(fileUpload).val('')
+          }
+        }
+        if (buildImageRequiredHeight) {
+          if (height !== buildImageRequiredHeight) {
+            alert('Please use an image that is ' + buildImageRequiredHeight + 'px high')
+            $(fileUpload).val('')
+          }
+        }
+      }
+    }
+  })
+
+  if (buildImageRequiredWidth || buildImageRequiredHeight) {
+    const $small = $('#event_image').closest('.form-group').find('small')
+    if (buildImageRequiredWidth && buildImageRequiredHeight) {
+      $small.text('Required image dimensions: ' + buildImageRequiredWidth + 'px x ' + buildImageRequiredHeight + 'px')
+    } else if (buildImageRequiredWidth) {
+      $small.text('Image must be ' + buildImageRequiredWidth + 'px wide')
+    } else if (buildImageRequiredHeight) {
+      $small.text('Image must be ' + buildImageRequiredHeight + 'px high')
+    }
+  }
+
+  // Zoom party toggle
+  $('#event_local_group_id').closest('.form-group').css('margin-bottom', '0.25rem')
+  $('#do-zoom-party a').click(function () {
+    $('#event_zoom_party').click()
+  })
+  $('#event_zoom_party').change(function () {
+    if ($(this).is(':checked')) {
+      $('#local-group-select').hide()
+      $('#do-zoom-party').hide()
+      $('#zoom-party-checkbox').show()
+    } else {
+      $('#local-group-select').show()
+      $('#do-zoom-party').show()
+      $('#zoom-party-checkbox').hide()
+    }
+  }).change()
+
+  // Currency symbol updates
+  $('#event_currency').change(function () {
+    if (typeof getCurrencySymbol !== 'undefined') {
+      $('.money-symbol').text(getCurrencySymbol($(this).val()))
+    }
+  })
+
+  // Donations UI toggle
+  $('#event_suggested_donation').keyup(function () {
+    if ($(this).val().length > 0) $('#donation-options').show()
+    else $('#donation-options').hide()
+  }).keyup()
+
+  // Disable controls for non-admin org members
+  if (!buildIsOrgAdmin) {
+    $('#do-zoom-party').hide()
+    $('input[name="event[zoom_party]"]').prop('disabled', true)
+    $('input[name="event[monthly_donors_only]"]').prop('disabled', true)
+    $('input[name="event[no_discounts]"]').prop('disabled', true)
+    $('input[name="event[include_in_parent]"]').prop('disabled', true)
+    $('input[name="event[affiliate_credit_percentage]"]').prop('disabled', true)
+    $('input[name="event[featured]"]').prop('disabled', true)
+    $('input[name="event[show_emails]"]').prop('disabled', true)
+    $('input[name="event[refund_deleted_orders]"]').prop('disabled', true)
+  }
+
+  // Revenue share UI if Stripe connected
+  if (buildRevenueSharingEnabled) {
+    $('#event_revenue_sharer_id').change(function () {
+      if ($(this).val().length > 0) {
+        $('#revenue-share').show()
+        $('#event_profit_share_to_organiser').val(0).closest('.form-group').hide()
+        $('#event_profit_share_to_coordinator, #event_profit_share_to_category_steward, #event_profit_share_to_social_media, #event_profit_share_to_organisation').parent().find('.input-group-text').text('/' + (100 - $('#event_revenue_share_to_revenue_sharer').val()))
+      } else {
+        $('#revenue-share').hide()
+        $('#event_profit_share_to_organiser').closest('.form-group').show()
+        $('#event_profit_share_to_coordinator, #event_profit_share_to_category_steward, #event_profit_share_to_social_media, #event_profit_share_to_organisation').parent().find('.input-group-text').text('%')
+      }
+    }).change()
+
+    $('#event_revenue_share_to_revenue_sharer').change(function () {
+      if ($('#event_revenue_sharer_id').val().length > 0) {
+        $('#event_profit_share_to_coordinator, #event_profit_share_to_category_steward, #event_profit_share_to_social_media, #event_profit_share_to_organisation').parent().find('.input-group-text').text('/' + (100 - $('#event_revenue_share_to_revenue_sharer').val()))
+      }
+    }).change()
+
+    $('#event_revenue_share_to_revenue_sharer, #event_profit_share_to_organiser, #event_profit_share_to_coordinator, #event_profit_share_to_category_steward, #event_profit_share_to_social_media, #event_profit_share_to_organisation')
+      .wrap('<div class="input-group" style="width: 10em"></div>')
+      .after('<div class="input-group-append"><span class="input-group-text">%</span></div>')
+      .change(function () {
+        const sum = $.map($('#event_revenue_share_to_revenue_sharer, #event_profit_share_to_organiser, #event_profit_share_to_coordinator, #event_profit_share_to_category_steward, #event_profit_share_to_social_media'), function (el) {
+          return parseInt($(el).val()) || 0
+        }).reduce(function (a, b) { return a + b })
+        const remaining = 100 - sum
+        $('#event_profit_share_to_organisation').val(remaining)
+      }).change()
+  }
+
+  // Prevent double-submit
+  $('#build-event').submit(function () {
+    $(this).find('button[type=submit]').prop('disabled', true)
+  })
+
+  // Live preview for questions
+  function loadQuestions () {
+    if (!eventId) return
+    $('#questions-preview').load('/events/' + eventId + '/questions?questions=' + encodeURIComponent($('#event_questions').val()), function () {
+      $('#questions-spinner').hide()
+    })
+  }
+  let questionsTimer
+  $('#event_questions').on('input', function () {
+    $('#questions-spinner').show()
+    clearTimeout(questionsTimer)
+    questionsTimer = setTimeout(function () { loadQuestions() }, 500)
+  })
+  loadQuestions()
+
+  // Live preview for feedback questions
+  function loadFeedbackQuestions () {
+    if (!eventId) return
+    $('#feedback-questions-preview').load('/events/' + eventId + '/feedback_questions?questions=' + encodeURIComponent($('#event_feedback_questions').val()), function () {
+      $('#feedback-questions-spinner').hide()
+    })
+  }
+  let feedbackQuestionsTimer
+  $('#event_feedback_questions').on('input', function () {
+    $('#feedback-questions-spinner').show()
+    clearTimeout(feedbackQuestionsTimer)
+    feedbackQuestionsTimer = setTimeout(function () { loadFeedbackQuestions() }, 500)
+  })
+  loadFeedbackQuestions()
+})
+
