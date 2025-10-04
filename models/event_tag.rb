940e6bdf1893bd483ce0cdfd2a7bf82caa969f1e
Stephen Reid <stephen@stephenreid.net>
2025-10-04 09:32:37 +0200

    ♻️ (event_tag.rb): refactor update_tags_for_select to reduce aggregation pipeline complexity

--- models/event_tag.rb
+++ models/event_tag.rb
@@ -21,16 +21,24 @@ class EventTag
 
   def self.update_tags_for_select
     fragment = Fragment.find_or_create_by(key: 'event_tags_for_select')
-    tag_names = EventTag.collection.aggregate([
-                                                { '$lookup': { from: 'event_tagships', localField: '_id', foreignField: 'event_tag_id', as: 'tagships' } },
-                                                { '$project': { name: 1, count: { '$size': '$tagships' } } },
-                                                { '$match': { count: { '$gt': 0 } } },
-                                                { '$sort': { count: -1, name: 1 } },
-                                                { '$limit': 500 }
-                                              ]).map do |doc|
-      tag_name = doc['name']
-      Sanitize.fragment(tag_name).gsub('&amp;', '&')
-    end.sort
+
+    tag_counts = EventTagship.collection.aggregate([
+                                                     { '$group': { _id: '$event_tag_id', count: { '$sum': 1 } } },
+                                                     { '$match': { count: { '$gt': 0 } } },
+                                                     { '$sort': { count: -1 } },
+                                                     { '$limit': 500 }
+                                                   ]).to_a
+
+    # Get the tag names for the top tags
+    tag_ids = tag_counts.map { |doc| doc['_id'] }
+    tags_by_id = EventTag.and(:id.in => tag_ids).pluck(:id, :name).to_h
+
+    # Build sorted list alphabetically
+    tag_names = tag_counts.map do |doc|
+      tag_name = tags_by_id[doc['_id']]
+      Sanitize.fragment(tag_name).gsub('&amp;', '&') if tag_name
+    end.compact.sort
+
     fragment.update_attributes expires: 1.day.from_now, value: tag_names.to_json
   end
 
