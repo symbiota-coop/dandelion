aeb1bea8070f5a3f519ccc813c331f24b36ab367
Stephen Reid <stephen@stephenreid.net>
2025-10-08 13:01:33 +0200

    🐛 (facilitators): filter map to only public accounts and cap at 500

--- app/controllers/facilitators.rb
+++ app/controllers/facilitators.rb
@@ -10,8 +10,15 @@ Dandelion::App.controller do
   get '/facilitators/map', provides: %i[html json] do
     @no_content_padding_bottom = true
     f = Fragment.find_by(key: 'facilitator_feedback_counts')
-    @account_ids_freq = JSON.parse(f.value)
-    @accounts = Account.and(:location_privacy => 'Public', :id.in => @account_ids_freq.map { |id, _freq| id })
+    account_ids_freq = JSON.parse(f.value)
+
+    # Get public accounts from all IDs
+    public_account_ids = Account.and(:location_privacy => 'Public', :id.in => account_ids_freq.map { |id, _freq| id }).pluck(:id).map(&:to_s)
+
+    # Filter to only public accounts and keep the sorting by count, then take top 500
+    public_account_ids_freq = account_ids_freq.select { |id, _freq| public_account_ids.include?(id) }.take(500)
+
+    @accounts = Account.and(:id.in => public_account_ids_freq.map { |id, _freq| id })
     case content_type
     when :html
       erb :'facilitators/map'
