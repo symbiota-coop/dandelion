817fc31dc3d85c5c490c628577b541e2ae7cd843
Stephen Reid <stephen@stephenreid.net>
2025-10-10 14:51:15 +0200

    ✨ (facilitators): extend search to also match against event tags

--- app/controllers/facilitators.rb
+++ app/controllers/facilitators.rb
@@ -1,7 +1,11 @@
 Dandelion::App.controller do
   get '/facilitators', provides: %i[html json] do
     @accounts = Account.and(:event_feedbacks_as_facilitator_count.gt => 0).order('event_feedbacks_as_facilitator_count desc')
-    @accounts = @accounts.and(:id.in => Account.search(params[:q], @accounts).pluck(:id)) if params[:q]
+    if params[:q]
+      ids_from_search = Account.search(params[:q], @accounts).pluck(:id)
+      ids_from_tags = @accounts.and(event_tags_joined: /#{Regexp.escape(params[:q])}/i).pluck(:id)
+      @accounts = @accounts.and(:id.in => ids_from_search + ids_from_tags)
+    end
     @accounts = @accounts.and(coordinates: { '$geoWithin' => { '$box' => @bounding_box } }) if params[:near] && %w[north south east west].all? { |p| params[p].nil? } && (@bounding_box = calculate_geographic_bounding_box(params[:near]))
 
     case content_type
