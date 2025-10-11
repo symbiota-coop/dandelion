caf6cb5d0d1b2584277a77938143099529073b71
Stephen Reid <stephen@stephenreid.net>
2025-10-11 18:33:33 +0200

    ♻️ Rename Rack::Attack rules for clarity and consistency

--- lib/rack_attack_throttle.rb
+++ lib/rack_attack_throttle.rb
@@ -5,11 +5,11 @@ Rack::Attack.cache.store = ActiveSupport::Cache::MongoStore.new(nil, collection:
 PROTECTED_PATH_PATTERNS = [%r{^/search$}, %r{^/events$}, %r{^/events\.ics$}, %r{^/o/[a-z0-9-]+/events$}, %r{^/o/[a-z0-9-]+/events\.ics$}].freeze
 
 # Completely block bots that try to use q parameter on protected paths
-Rack::Attack.blocklist('block bots using q param on protected paths') do |request|
+Rack::Attack.blocklist('crawlers on protected paths using q param') do |request|
   request.is_crawler? && PROTECTED_PATH_PATTERNS.any? { |pattern| request.path.match?(pattern) } && request.params['q']
 end
 
 # Throttle other bot/crawler requests to protected paths
-Rack::Attack.throttle('bots and crawlers', limit: 1, period: 1.minute) do |request|
+Rack::Attack.throttle('crawlers on protected paths', limit: 1, period: 1.minute) do |request|
   request.user_agent if request.is_crawler? && PROTECTED_PATH_PATTERNS.any? { |pattern| request.path.match?(pattern) }
 end
