ccbcaf8ecf43eeb5e40f946d31a566c0857d68ac
Stephen Reid <stephen@stephenreid.net>
2025-10-20 22:28:34 +0200

    ✏️ Fix typo in feedback_email_body placeholder

--- models/concerns/email_fields.rb
+++ models/concerns/email_fields.rb
@@ -52,7 +52,7 @@ module EmailFields
         reminder_email_title: 'Custom subject line for the reminder email',
         reminder_email_body: 'Custom body for the reminder email',
         feedback_email_title: 'Custom subject line for the feedback request email',
-        feedback_email_body: 'Custom body for the feedback request email. Make sure to include a link to [feedback url]'
+        feedback_email_body: 'Custom body for the feedback request email. Make sure to include a link to [feedback_url]'
       }
     end
   end
