7fcb59a71343620a6dfad0a404f6eaf4d82f0350
Stephen Reid <stephen@stephenreid.net>
2025-11-07 10:47:31 +0100

    🐛 (app.js): add confirmation prompt for duplicate links

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -76,11 +76,18 @@ $(function () {
       }
     })
 
-    $('[data-confirm], [href$="destroy"]').not('[data-confirm-registered]').attr('data-confirm-registered', 'true').each(function () {
+    $('[data-confirm], [href$="destroy"], [href$="duplicate"]').not('[data-confirm-registered]').attr('data-confirm-registered', 'true').each(function () {
       $(this).click(function () {
         $(this).removeClass('no-trigger')
 
-        const message = $(this).data('confirm') || 'Are you sure?'
+        let message = $(this).data('confirm')
+        if (!message) {
+          if ($(this).attr('href') && $(this).attr('href').endsWith('duplicate')) {
+            message = 'Are you sure you want to duplicate the event?'
+          } else {
+            message = 'Are you sure?'
+          }
+        }
         if (!confirm(message)) {
           $(this).addClass('no-trigger')
           return false
