4447deb1e6bc7deda69ae7bf3db63e86db9562fe
Stephen Reid <stephen@stephenreid.net>
2025-11-27 22:41:28 +0100

    🐛 Fix duplicate connections in recommendation cache

--- models/account_recommendation_cache.rb
+++ models/account_recommendation_cache.rb
@@ -24,7 +24,7 @@ class AccountRecommendationCache
     people = {}
 
     events.each do |event|
-      event.attendee_ids.each do |attendee_id|
+      event.attendee_ids.uniq.each do |attendee_id|
         next if attendee_id == account_id
 
         connection = { type: 'Event', id: event.id.to_s }
@@ -39,7 +39,7 @@ class AccountRecommendationCache
     # Get connections from gatherings
     gatherings = Gathering.and(:id.in => account.memberships.pluck(:gathering_id))
     gatherings.each do |gathering|
-      gathering.member_ids.each do |member_id|
+      gathering.member_ids.uniq.each do |member_id|
         next if member_id == account_id
 
         connection = { type: 'Gathering', id: gathering.id.to_s }
