1c4531f7ef0693382281b505c6481d24f99fac80
Stephen Reid <stephen@stephenreid.net>
2025-12-13 09:45:30 +0100

    🔧 (honeybadger-claude.yml): switch trigger from user login to issue title pattern 🔧 (honeybadger_init.rb): add Rack::Multipart::EmptyContentError to ignored errors

--- lib/honeybadger_init.rb
+++ lib/honeybadger_init.rb
@@ -7,6 +7,7 @@ Honeybadger.configure do |config|
       %w[Sinatra::NotFound SignalException].include?(error_type),
       error_type == 'Sinatra::BadRequest' && error_message && error_message.include?('invalid %-encoding'),
       error_type == 'Sinatra::BadRequest' && error_message && error_message.include?('Invalid multipart/form-data: EOFError'),
+      error_type == 'Sinatra::BadRequest' && error_message && error_message.include?('Invalid multipart/form-data: Rack::Multipart::EmptyContentError'),
       error_type == 'ThreadError' && error_message && error_message.include?("can't be called from trap context"),
       error_type == 'Mongoid::Errors::Validations' && error_message && error_message.include?('Ticket type is full'),
       error_type == 'Mongoid::Errors::Validations' && error_message && error_message.include?('Ticket type is not available as sales have ended'),
