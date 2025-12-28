197407f2cbcadc62924cc5c1a23f46e263a057cb
Stephen Reid <stephen@stephenreid.net>
2025-12-28 11:35:12 +0100

    🐛 Fix pagination_details helper to show clearer messages

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -27,7 +27,18 @@ Dandelion::App.helpers do
   end
 
   def pagination_details(collection, model: nil)
-    page_entries_info(collection, model: model)
+    if collection.total_pages < 2
+      case collection.to_a.length
+      when 0
+        "No #{model.pluralize.downcase} found"
+      when 1
+        "Displaying <b>1</b> #{model.downcase}"
+      else
+        "Displaying <b>all #{collection.count}</b> #{model.pluralize.downcase}"
+      end
+    else
+      "Displaying #{model.pluralize.downcase} <b>#{collection.offset + 1} - #{collection.offset + collection.to_a.length}</b> of <b>#{collection.count}</b> in total"
+    end
   end
 
   def set_time_zone
