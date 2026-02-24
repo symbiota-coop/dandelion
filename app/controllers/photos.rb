59912a62c35d3fdda61373e365b88126234feadd
Stephen Reid <stephen@stephenreid.net>
2026-02-24 14:47:29 +0100

    ♻️ (photos.rb): replace halt(404) with not_found helper method for consistency

--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -10,7 +10,7 @@ Dandelion::App.controller do
     photoable_type = params[:photoable_type].to_s
     halt(403) unless Photo.photoable_types.include?(photoable_type)
 
-    photoable = photoable_type.constantize.find(params[:photoable_id]) || halt(404)
+    photoable = photoable_type.constantize.find(params[:photoable_id]) || not_found
     halt(403) unless can_add_photo_to?(photoable, current_account)
 
     @photo = Photo.create!(image: params[:image], account: current_account, photoable_type: photoable_type, photoable_id: photoable.id)
