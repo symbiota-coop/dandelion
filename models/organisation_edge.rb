9163050ccc5de7b6c71dd62e4faf7130c564bb26
Stephen Reid <stephen@stephenreid.net>
2026-04-02 07:46:23 +0200

    ⚡️ (organisation_edge.rb): optimize create_all method with bulk aggregation and batch inserts instead of nested queries

--- models/organisation_edge.rb
+++ models/organisation_edge.rb
@@ -11,13 +11,42 @@ class OrganisationEdge
   validates_uniqueness_of :sink, scope: :source
 
   def self.create_all(organisations)
-    organisations.each do |source|
-      organisations.each do |sink|
-        next if source.id == sink.id
-        next if OrganisationEdge.find_by(source: sink, sink: source)
+    org_ids = organisations.pluck(:id)
+    return if org_ids.empty?
 
-        OrganisationEdge.create(source: source, sink: sink, mutual_followers: source.members.and(:id.in => sink.members.pluck(:id)).count)
-      end
+    org_members = Account.collection.aggregate([
+      { '$match' => { 'organisation_ids_cache' => { '$in' => org_ids } } },
+      { '$project' => {
+        'orgs' => {
+          '$filter' => {
+            'input' => '$organisation_ids_cache',
+            'cond' => { '$in' => ['$$this', org_ids] }
+          }
+        }
+      } },
+      { '$unwind' => '$orgs' },
+      { '$group' => { '_id' => '$orgs', 'member_ids' => { '$addToSet' => '$_id' } } }
+    ]).to_h { |doc| [doc['_id'], doc['member_ids'].to_set] }
+
+    now = Time.now
+    edges = []
+
+    org_ids.combination(2).each do |source_id, sink_id|
+      source_set = org_members[source_id]
+      sink_set = org_members[sink_id]
+      mutual = source_set && sink_set ? (source_set & sink_set).size : 0
+      next if mutual.zero?
+
+      edges << {
+        _id: BSON::ObjectId.new,
+        source_id: source_id,
+        sink_id: sink_id,
+        mutual_followers: mutual,
+        created_at: now,
+        updated_at: now
+      }
     end
+
+    collection.insert_many(edges) if edges.any?
   end
 end
