6fd3903e85a3abd643c9427ea9655276f86158f3
Stephen Reid <stephen@stephenreid.net>
2026-05-10 19:58:41 +0200

    🐛 Fix minimal subscribe flow in iframe context

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -54,6 +54,9 @@ Dandelion::App.helpers do
     elsif params[:organisation_id]
       @organisation ||= Organisation.find(params[:organisation_id])
       @account.associate_with_organisation!(@organisation, skip_welcome: params[:skip_welcome])
+
+      halt 200, "<script>window.parentIFrame.sendMessage('subscribed');</script>" if params[:minimal_subscribe]
+
       redirect "/accounts/edit?organisation_id=#{@organisation.id}"
     elsif params[:activity_id] || params[:local_group_id] || params[:event_id]
       associate_account_with_context(@account)
